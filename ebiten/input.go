d1cd916988de3f0010aac367e68cdcbd87e7f4c7
Ben Eatock <bdeatock@gmail.com>
2025-03-14 08:38:53 +0000

    Refactor: extract step cycle input check to own fn

--- ebiten/input.go
+++ ebiten/input.go
@@ -16,11 +16,17 @@ func (g *Game) handleInput() error {
 		}
 	}
 
-	if !g.stepMode || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) || inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+	if !g.stepMode || g.inputForStepCycle() {
 		g.cycleCount++
 		deltaTime := time.Second / time.Duration(g.cyclesPerSecond)
 		return g.emulator.RunCycle(deltaTime)
 	}
 
 	return nil
+}
+
+func (g *Game) inputForStepCycle() bool {
+	return inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) ||
+		inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) ||
+		inpututil.IsKeyJustPressed(ebiten.KeySpace)
 }
